d568aee08d9f3554e0ac13841ef3d4ab7b3e8aff
Xxploiter <dipankarthedestroyer@gmail.com>
2022-04-03 11:40:11 +0530

    Now added some tests and added a simple serve, html file to get the data from the json file

--- src/db.js
+++ src/db.js
@@ -1,18 +1,33 @@
 import fs from 'node:fs/promises';
-const DB_PATH = new URL('./db.json', import.meta.url).pathname;
+// const DB_PATH = new URL('../db.json', import.meta.url).pathname; // BUG this doest work in windows
+// console.log(DB_PATH);
+import path from 'node:path';
 
+const DB_FILENAME = 'db.json'; // File name
+const DB_DIR = './'; // Directory where db.json should be located
 
+// Construct the full path to db.json
+const DB_PATH = path.join(DB_DIR, DB_FILENAME);
 // the function below is a helper function to read the db.json file it 
 // reads the file and returns the parsed JSON object
 export const getDB = async () => {
     try {
+      // Try to read the existing database file.
+      const db = await fs.readFile(DB_PATH, 'utf-8');
+      return JSON.parse(db);
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        // If the file doesn't exist, create it with an empty database.
+        await fs.writeFile(DB_PATH, JSON.stringify({ notes: [] }, null, 2));
+        // Now that the file has been created, read and return its content.
         const db = await fs.readFile(DB_PATH, 'utf-8');
         return JSON.parse(db);
-    } catch (e) {
-        console.log('error in getDB', e);
+      } else {
+        // If there's an error other than "file not found," re-throw it.
+        throw error;
+      }
     }
-}
-
+  }
 // the function below takes in a db object and writes it to the db.json file
 export const saveDB = async (db) => {
     await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
@@ -21,7 +36,7 @@ export const saveDB = async (db) => {
 
 // uses the getDB function to read the current db.json file,
 // then adds the new note to the notes array, and then uses the saveDB function
-export const insertDB = async (note) => {
+export const insert = async (note) => {
     const db = await getDB();
     db.notes.push(note);
     await saveDB(db);
--- src/notes.js
+++ src/notes.js
@@ -1,39 +1,36 @@
-import { insertDB, getDB, saveDB } from "./db";
+import { insert, getDB, saveDB } from './db.js'
 
 export const newNote = async (note, tags) => {
-  const newNote = {
+  const data = {
     tags,
-    id: Date.now(),
     content: note,
-  };
-  await insertDB(newNote);
-  return newNote;
-};
-
+    id: Date.now(),
+  }
+  await insert(data)
+  return data
+}
 export const getAllNotes = async () => {
-  const { notes } = await getDB();
-  return notes;
-};
-
-export const findNotes = async (searchWords) => {
-    const { notes } = await getDB(); 
-    return notes.filter(note => note.content.toLowerCase().includes(searchWords.toLowerCase()));
+  const db = await getDB()
+  return db.notes
 }
 
+export const findNotes = async (filter) => {
+  const notes = await getAllNotes()
+  return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
+}
 
 export const removeNote = async (id) => {
-    const {notes} = await getDB();
-    const match = notes.find(note => note.id === id);
+  const notes = await getAllNotes()
+  const match = notes.find(note => note.id === id)
 
-    if(match){
-        const newNotes = notes.filter(note => note.id !== id);
-        await saveDB({notes: newNotes});
-        return id;
-    }
+  if (match) {
+    const newNotes = notes.filter(note => note.id !== id)
+    await saveDB({notes: newNotes})
+    return id
+  }
 }
 
-// export const removeAllNotes = async () => {
-//     await saveDB({notes: []});
-//     return true;
-// }
-export const removeAllNotes = () => saveDB({notes: []});
+export const removeAllNotes = async () => {
+  await saveDB({notes: []})
+}
+
